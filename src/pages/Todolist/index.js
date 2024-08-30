125c87f4430ade51beaed4373ff5a58c3224ed29
poudelsuk <sukadevpoudel2@gmail.com>
2024-08-30 17:53:07 +0545

    todo

---
+++ src/pages/Todolist/index.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+export function TodoList(){
+    const [tasks,setTasks]=useState(["do homework","eat rice"]);
+    const [inputTask,setinputTask]=useState();
+    const handleInputNewTask=(e)=>{
+       const value=e.target.value; 
+       setinputTask(value);
+
+    }
+    const handleAddNewTask=()=>{
+      setTasks([...tasks,inputTask]);
+    };
+    return(
+        <div>
+            <h1>Task Manager</h1>
+            <label>
+                Enter task
+                <input onInput={handleInputNewTask}/>
+            </label>
+            <button onClick={handleAddNewTask}>add task</button>
+            <ul>
+      {tasks.map((task)=>{
+        return( <li>{task}</li>);
+       
+      //return <HistoryItem count={c}/>
+       
+      
+       } )}
+      </ul>
+        </div>
+        
+    );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import { useState } from "react";
 import MyButton from "./my-button";
 import History from "./history";
+import { TodoList } from "./Todolist";
 const max_counter=10;
 const min_counter=-10;
 
@@ -78,7 +79,8 @@ export default function Home() {
      <History
      history={history}
 />
-   
+   <TodoList
+   />
 
       </main>
     </>
